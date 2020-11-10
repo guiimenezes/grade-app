d9cf2c830ac1c22730b1e10dfa564ef35bfcb815
Guilherme Matos de Menezes <guiimenezes.ti@gmail.com>
2020-11-09 23:11:00 -0300

    alteração do http commons

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: 'https://grade-node-mongodb.herokuapp.com/grade',
+  baseURL: 'http://localhost:8081',
   headers: {
     'Content-type': 'application/json',
   },
